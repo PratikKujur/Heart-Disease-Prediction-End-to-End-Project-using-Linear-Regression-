9a883955e6072cbc727c7cbdc6a22b6edf2c59e6
PratikKujur <pratikkujur121@gmail.com>
2024-09-29 12:24:38 +0530

    constants updated

--- template.py
+++ template.py
@@ -7,6 +7,7 @@ logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:%(message)s')
 project_name='LogisticRegression'
 
 list_of_files=[
+    ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
@@ -15,15 +16,20 @@ list_of_files=[
     f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "dvc.yaml",
     "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "app.py",
+    "Dockerfile",
     "requirements.txt",
     "setup.py",
-    "main.py",
-    "research/trails.ipynb",
-    "templates/index.html"
+    "research/trials.ipynb",
+    "templates/index.html",
+    "test.py"
 ]
 
 for filepath in list_of_files:
