13cba117dcbfac4ef5b8a353e3f6cd03847b61c8
PratikKujur <pratikkujur121@gmail.com>
2024-10-02 16:28:14 +0530

    dataset updated

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from LogisticRegression import logger
 from LogisticRegression.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from LogisticRegression.pipeline.stage_02_data_validation import DataValidationPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         data_ingestion = DataIngestionTrainingPipeline()
         data_ingestion.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        data_validation = DataValidationPipeline()
+        data_validation.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
